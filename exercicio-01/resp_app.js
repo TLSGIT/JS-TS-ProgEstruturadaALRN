65a853c398ed7bb29772b76778c536353ff411cd
T3L35 <teles.tux@gmail.com>
2024-03-15 08:40:21 -0300

    First commit with answers.

---
+++ exercicio-01/resp_app.js
@@ -0,0 +1,12 @@
+// 05
+const myBirthYear2 = 1989
+// 06
+const currentYear2 = 2023
+// 07
+console.log(myBirthYear2, currentYear2)
+// 08
+let myAge2 = currentYear2 - myBirthYear2
+console.log(myAge2)
+// 09
+const ten2 = 10
+console.log(`Em ${currentYear2 + ten}, estarei com ${myAge2 + ten2} anos de idade.`)
---
+++ exercicio-07/app.js
@@ -0,0 +1,167 @@
+/*
+  01
+
+  - Inverta o valor dos booleans do console.log() abaixo;
+  - O resultado exibido no console deve ser: false true.
+*/
+
+console.log(!true, !false)
+
+/*
+  02
+
+  - Abaixo do array "animals", verifique se o animal "leão" **não** existe no  
+    array. Se não existir, exiba no console a mensagem "Leão não existe no array  
+    animals.";
+  - Se existir, exiba no console a mensagem "Existe um leão no array animals.".
+*/
+
+const animals = ['macaco', 'tucano', 'elefante', 'pavão', 'hipopótamo']
+
+console.log(!animals.includes("leão") ? "Leão não existe no array animals." : "Existe um leão no array animals.")
+
+/*
+  03
+
+  - Some os números do array abaixo;
+  - Se durante a soma, o resultado ultrapassar 400, pare a operação e exiba  
+    a frase abaixo no console, substituindo "RESULTADO_DA_SOMA" pelo valor  
+    correto.
+  
+  "A soma ultrapassou 400. Até aqui, o valor atual é RESULTADO_DA_SOMA."
+*/
+
+const randomNumbers = [59, 61, 73, 57, 35, 73, 21, 87, 43]
+let sumArray = 0
+for (let i = 0; i < randomNumbers.length; i++) {
+  if (sumArray >= 400) { console.log(`A soma ultrapassou 400. Até aqui, o valor atual é ${sumArray}.\nE eu parei de somar no número: ${randomNumbers[i - 1]}.`) }
+  else { sumArray += randomNumbers[i] }
+}
+/*
+  04
+
+  - Concatene as strings do array abaixo, formando uma frase;
+  - Se durante a concatenação a palavra "certeza" existir, ela não deve ser  
+    concatenada;
+  - Exiba a frase no console.
+*/
+
+const sentence = ['A', 'certeza', 'dúvida', 'é', 'o', 'princípio', 'da', 'sabedoria.']
+
+let fraseConcat = ''
+for (let i = 0; i < sentence.length; i++) {
+  if (sentence[i] != 'certeza') { fraseConcat += (sentence[i] + ' ') }
+  else { console.log('Não concat...' + i) }
+}
+console.log(fraseConcat.length)
+console.log(fraseConcat)
+
+/*
+  05
+
+  - Itere sobre o array "randomValues" apenas até a 4ª string dele;
+  - Exiba a string abaixo no console, mantendo a formatação de lista e inserindo  
+    as informações corretas:
+
+  "
+    3 informações sobre o array randomValues:
+      - As primeiras 4 strings são XX, XX, XX e XX;
+      - Até que as primeiras 4 strings fossem iteradas, XX booleans foram iterados;
+      - O array foi iterado por XX vezes.
+  "
+*/
+
+const randomValues = [57, false, 'JS', [], true, 'HTML', 31, null, false, 'CSS', 97, true, 'Git', 11, 'sticker', false, 'GitHub', true, null]
+let sepStrings = []
+let findBoolens = 0, iterations = 0
+
+console.log(typeof (randomValues[0]) + typeof (randomValues[1]) + typeof (randomValues[2]) + typeof (randomValues[3]))
+
+for (let i = 0; i < randomValues.length; i++) {
+  if (typeof (randomValues[i]) == 'string') { sepStrings.push(randomValues[i]) }
+  else if (typeof (randomValues[i]) == 'boolean') { }
+  else {
+    if (sepStrings.length == 4) {
+      break
+    }
+    iterations = i + 1
+  }
+}
+console.log(`As primeiras 4 strings são: ${sepStrings.join(", ")}.`)
+console.log(`O array foi iterado por ${iterations} vezes.`)
+
+/*
+  06
+
+  - Descomente a constante abaixo atribua a ela algum tipo de bebida. Exemplo:  
+    água, refrigerante ou suco;
+  - Utilize um switch statement com cases para essas 3 possibilidades de bebida;
+  - Se o tipo da bebida é água, atribua à uma variável a mensagem "Substância 
+    química cujas moléculas são formadas por dois átomos de hidrogênio e um de 
+    oxigênio.";
+  - Se o tipo da bebida é refrigerante, a mensagem "Bebida não alcoólica e não 
+    fermentada, fabricada industrialmente, à base de água mineral e açúcar." 
+    deve ser armazenada;
+  - Se é suco, atribua a mensagem "Bebida produzida do líquido extraído de 
+    frutos.";
+  - Se nenhum desses 3 tipos de bebida der match, a variável deve armazenar 
+    "Bebida desconhecida.";
+  - Exiba a mensagem no console. Teste também as outras possibilidades de tipo  
+    da bebida além da que você escolheu.
+*/
+
+const drinkType = 'Água'
+let typeIdentified = ''
+const messagesDrinks = ['Substância química cujas moléculas são formadas por dois átomos de hidrogênio e um de oxigênio.',
+  'Bebida não alcoólica e não fermentada, fabricada industrialmente, à base de água mineral e açúcar.',
+  'Bebida produzida do líquido extraído de frutos.',
+  'Bebida desconhecida.']
+
+switch (drinkType) {
+  case "Água":
+    typeIdentified = messagesDrinks[0]
+    break
+  case "Refrigerante":
+    typeIdentified = messagesDrinks[1]
+    break
+  case "Suco":
+    typeIdentified = messagesDrinks[2]
+    break
+  default:
+    typeIdentified = messagesDrinks[3]
+    break
+}
+
+console.log(`O tipo de bebida identificada foi: ${typeIdentified}`)
+
+/*
+  07
+
+  - Reescreva o código comentado abaixo, utilizando um switch statement;
+  - Depois de escrever o switch, modifique o valor da declaração da const "a"  
+    para testar o switch que você escreveu.
+*/
+
+const a = 2
+
+// if (a === 0) {
+//   console.log(`O valor de "a" é ${a}`)
+// } else if (a === 1) {
+//   console.log(`O valor de "a" é ${a}`)
+// } else {
+//   console.log('O valor de "a" é qualquer número, exceto 0 e 1')
+// }
+
+switch (a) {
+  case 0:
+    console.log(`O valor de "a" é ${a}`)
+    break
+  case 1:
+    console.log(`O valor de "a" é ${a}`)
+    break
+  case 2:
+    console.log(`O valor de "a" é ${a}`)
+    break
+  default:
+    console.log(`O valor de "a" é um valor não previsto.`)
+}
